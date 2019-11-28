83837306262059326e0ad6a7bad514c9d0ee382e
Dyne Estudio <40637572+dynestudio@users.noreply.github.com>
2019-11-28 12:11:24 -0300

    Update URP dialog module v0.2

--- references/dialog_URP_render path_v01.py
+++ references/dialog_URP_render path_v01.py
@@ -1,73 +1,82 @@
 import c4d
 from c4d import gui
 
-IDC_LABELNAME     = 10000
-IDC_LIST          = 10002
-IDC_USERINPUT_00  = 10003
-IDC_USERINPUT_01  = 10004
-IDC_USERINPUT_02  = 10005
-IDC_POINTS        = 10007
-IDC_POINTSOP01    = 10008
-IDC_POINTSOP02    = 10009
-
 class OptionsDialog(gui.GeDialog):
+    IDC_GROUP_01         = 10000
+    IDC_LABELNAME_01     = 10010
+    IDC_LABELNAME_02     = 10020
+    IDC_LIST_01          = 10030
+    IDC_LIST_01_INPUT01  = 10040
+    IDC_LIST_01_INPUT02  = 10050
+    IDC_LIST_02          = 10060
+    IDC_LIST_02_INPUT01  = 10070
+    IDC_LIST_02_INPUT02  = 10080
+    IDC_LIST_02_INPUT03  = 10090
+    STR_RNDR_TYPE01      = "Still"
+    STR_RNDR_TYPE02      = "Animation"
+    STR_PATH_TYPE01      = "Drive Path"
+    STR_PATH_TYPE02      = "Relative Project Path"
+    STR_PATH_TYPE03      = "Team Server Path"
+
     def CreateLayout(self):
         # title
         self.SetTitle('Update Render Path')
 
+        # group colums
+        self.GroupBegin(self.IDC_GROUP_01, c4d.BFH_CENTER, 2, 2, "Main Group", 0, 100, 10)
+
         # statics text - description UI
-        self.AddStaticText(IDC_LABELNAME, c4d.BFH_LEFT, name = 'Select render path type:') 
-
+        self.AddStaticText(self.IDC_LABELNAME_01, c4d.BFH_LEFT, name = 'Select render type . . . .') 
         # combo box UI - render type
-        self.AddComboBox(IDC_POINTS, c4d.BFH_CENTER, initw = 100, inith = 10, specialalign = True)
-        self.AddChild(IDC_POINTS, IDC_POINTSOP01, "Still")
-        self.AddChild(IDC_POINTS, IDC_POINTSOP02, "Animation")
-
+        self.AddComboBox(self.IDC_LIST_01, c4d.BFH_CENTER, initw = 200, inith = 12, specialalign = False)
+        self.AddChild(self.IDC_LIST_01, self.IDC_LIST_01_INPUT01, self.STR_RNDR_TYPE01)
+        self.AddChild(self.IDC_LIST_01, self.IDC_LIST_01_INPUT02, self.STR_RNDR_TYPE02)
+        
+        # statics text - description UI
+        self.AddStaticText(self.IDC_LABELNAME_02, c4d.BFH_LEFT, name = 'Select path type . . . . . .') 
         # combo box UI - path type
-        self.AddComboBox(IDC_LIST, c4d.BFH_CENTER, initw = 100, inith = 10, specialalign = True)
-        self.AddChild(IDC_LIST, IDC_USERINPUT_00, "Drive Path")
-        self.AddChild(IDC_LIST, IDC_USERINPUT_01, "Project Path")
-        self.AddChild(IDC_LIST, IDC_USERINPUT_02, "Team Server Path")
+        self.AddComboBox(self.IDC_LIST_02, c4d.BFH_CENTER, initw = 200, inith = 11, specialalign = False)
+        self.AddChild(self.IDC_LIST_02, self.IDC_LIST_02_INPUT01, self.STR_PATH_TYPE01)
+        self.AddChild(self.IDC_LIST_02, self.IDC_LIST_02_INPUT02, self.STR_PATH_TYPE02)
+        self.AddChild(self.IDC_LIST_02, self.IDC_LIST_02_INPUT03, self.STR_PATH_TYPE03)
+        
+        # close group
+        self.GroupEnd()
 
         # Ok/Cancel buttons
         self.AddDlgGroup(c4d.DLG_OK|c4d.DLG_CANCEL)
         self.ok = False
 
         # set dialog default values
-        self.SetInt32(IDC_LIST , IDC_USERINPUT_00)
-        self.SetInt32(IDC_POINTS, IDC_POINTSOP01)
+        self.SetInt32(self.IDC_LIST_01 , self.IDC_LIST_01_INPUT01)
+        self.SetInt32(self.IDC_LIST_02 , self.IDC_LIST_02_INPUT01)
 
         return True
 
     def Command(self, id, msg):
         if id == c4d.IDC_OK:
             self.ok = True
-            self.findCName = self.GetInt32(IDC_LIST)
+            self.FIND_RNDR_TYPE = self.GetInt32(self.IDC_LIST_01)
+            self.FIND_RNDR_PATH = self.GetInt32(self.IDC_LIST_02)
             self.Close()
-
         elif id == c4d.IDC_CANCEL:
-            self.Close()
-            
+            self.Close()      
 
         return True
 
 def main():
-    # Open the options dialog to let users choose their options.
+    # open the options dialog to let users choose their options.
     dlg = OptionsDialog()
     dlg.Open(c4d.DLG_TYPE_MODAL, defaultw = 300, defaulth = 50)
     if not dlg.ok:
         return
 
-    '''dialog = dlg.findCName
-    pdialog = dlg.findPName
+    dlg_rndr_type = dlg.FIND_RNDR_TYPE
+    dlg_rndr_path = dlg.FIND_RNDR_PATH
 
-    if pdialog == IDC_POINTSOP01:
-        pdialog = True
-    else:
-        pdialog = False
-
-    print dialog
-    print pdialog '''
+    print dlg_rndr_type
+    print dlg_rndr_path
+    print dlg.IDC_LIST_01_INPUT02
 
 if __name__=='__main__':
  main()
